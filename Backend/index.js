ae7adf699bfb426f2062344ba78482f8cc05ee08
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-03-08 09:08:30 +0530

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
@@ -21,12 +21,7 @@ const cors = require("cors");
 
 dbConnect();
 app.use(morgan("dev"));
-app.use(cors({
-  origin: 'https://prabanjampgm.com', // Allow requests from this origin 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // If you're sending cookies/auth headers
-}));
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
