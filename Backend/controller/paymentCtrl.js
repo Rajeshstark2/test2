0ed9a332418d4650f82980424dae19556e4a4653
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-03-16 10:08:15 +0530

    Update paymentCtrl.js

--- Backend/controller/paymentCtrl.js
+++ Backend/controller/paymentCtrl.js
@@ -1,8 +1,9 @@
-const Cashfree = require("cashfree-sdk");
-Cashfree.Payouts.init({
-  key_id: "920442d64d234896cf1df8f22a244029",
-  key_secret: "cfsk_ma_prod_e9b9aa6f6b77f4fd782e53b02b7313bc_e7518d81",
-});
+const { Cashfree } = require("@cashfreepayments/cashfree-sdk");
+
+// Initialize Cashfree PG SDK
+Cashfree.XClientId = "920442d64d234896cf1df8f22a244029"; // Replace with your client ID
+Cashfree.XClientSecret = "cfsk_ma_prod_e9b9aa6f6b77f4fd782e53b02b7313bc_e7518d81"; // Replace with your client secret
+Cashfree.XEnvironment = Cashfree.Environment.Production;
 
 const checkout = async (req, res) => {
   const { amount } = req.body;
