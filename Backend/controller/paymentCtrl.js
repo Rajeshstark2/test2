a549f1c65cc1449ae92d62fdff1d1c8f98f8bc79
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-03-16 09:53:11 +0530

    Update paymentCtrl.js

--- Backend/controller/paymentCtrl.js
+++ Backend/controller/paymentCtrl.js
@@ -1,4 +1,4 @@
-const Razorpay = require("razorpay");
+const Razorpay = require("cashfree-sdk");
 const instance = new Razorpay({
   key_id: "920442d64d234896cf1df8f22a244029",
   key_secret: "cfsk_ma_prod_e9b9aa6f6b77f4fd782e53b02b7313bc_e7518d81",
