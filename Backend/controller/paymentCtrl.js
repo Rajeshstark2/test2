24f0ecbb808f6cd6d09811561f9dbe5e59a4a409
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-03-16 09:53:30 +0530

    Update paymentCtrl.js

--- Backend/controller/paymentCtrl.js
+++ Backend/controller/paymentCtrl.js
@@ -7,7 +7,7 @@ const instance = new Razorpay({
 const checkout = async (req, res) => {
   const { amount } = req.body;
   const option = {
-    amount: amount * 100,
+    amount: amount * 50,
     currency: "INR",
   };
   const order = await instance.orders.create(option);
